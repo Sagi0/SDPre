232c96db7217a386f1ba9ce47607c6d31a1ed792
Julian Pettersen <julian1pettersen@gmail.com>
2022-09-28 10:59:30 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+
+    double a = 8.0;
+    double b = 10.0;
+    double hypotenuse = Math.sqrt((a*a)+(b*b));
+
+    System.out.println("The hypotenuse of a triangle with sides " + b + " and " + a + " is " + hypotenuse);
+    
+  }
+}

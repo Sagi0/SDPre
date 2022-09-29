8668c445f13ea1b38f793576a7ea80d031abb8b5
Julian Pettersen <julian1pettersen@gmail.com>
2022-09-29 15:49:53 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,18 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 0;
+    int sum = 0;
+    int count;
+
+    while(counter<=20) {
+      count = counter;
+      sum = sum + count;
+      counter = counter + 1;
+
+    if(sum>=210) {
+      System.out.println(sum);
+      
+      }
+    }
+  }
+}

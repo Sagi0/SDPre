7ac580ea3eca2aa6ca910b160d32b6de03709b98
Julian Pettersen <julian1pettersen@gmail.com>
2022-10-01 14:22:10 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,20 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+    int i;
+
+    for(i=1; i<=20;) {
+
+    System.out.print(i);
+
+    if(i % 2 == 0)
+      System.out.println(" is even");
+
+    if(i % 2 == 1)
+      System.out.println(" is odd");
+
+      i++;
+
+    }
+  }
+}
